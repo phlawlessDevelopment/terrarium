cb0a56f2817645136abec6ca8d63a348beb6b105
phlawlessDevelopment <phalyce@outlook.com>
2022-05-29 22:34:21 +0100

    rendered glass jar (material not working)

--- builder/models.py
+++ builder/models.py
@@ -3,8 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Jar3DModel(models.Model):
+    
     model_name = models.CharField(max_length=16)
     file = models.FileField()
+    material = models.FileField()
 
     def __str__(self) -> str:
         return f'{self.model_name}'
--- builder/views.py
+++ builder/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 from builder.models import Jar3DModel
 
 def builder(request):
-    jar_url = Jar3DModel.objects.first().file.url
-    context = {'jar_url':jar_url}
-    print(jar_url)
+    jar = Jar3DModel.objects.first()
+    jar_name = jar.file.name
+    mat_name = jar.material.name
+    print(jar_name)
+    print(mat_name)
+    context = {'jar_name':jar_name , 'mat_name':mat_name}
     return render(request, template_name='builder/builder.html', context=context)
